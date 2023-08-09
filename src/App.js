dd0b9a1736b0f79589565bc8c7b58fdb37035807
kanybek <duisheev.kanybek@gmail.com>
2023-08-08 21:30:39 -0500

    redux files adding

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+// import { useSelector } from 'react-redux';
+import VotingApp from "./component/votingApp"
+import Submit from "./component/submit";
+// import store from "./redux/store"
 import './App.css';
 
 function App() {
+  // const programmingLangs = useSelector(data => data)
+  // console.log(programmingLangs, "<<<<<<<<")
+  // store.getState()
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>
+        Voting App
+      </h3>
+      <VotingApp />
+      <Submit />
+
     </div>
   );
 }
